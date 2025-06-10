335dfbf82b1782f7b4a395ba2fc34ccac07f2508
LuisHRodrigues <luishenriquebr505@gmail.com>
2025-06-10 19:07:30 -0300

    Adiconar o DTO do Paciente

---
+++ consulta-api/consuta/src/main/java/com/example/consulta/dto/PacienteRequestDTO.java
@@ -0,0 +1,27 @@
+package com.example.consulta.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+/**
+ * DTO para receber os dados de criação/atualização de um Paciente.
+ */
+public record PacienteRequestDTO(
+        @NotBlank(message = "O nome não pode ser vazio") String nome,
+
+        @NotBlank(message = "O nome de usuário não pode ser vazio") String username,
+
+        @NotBlank(message = "A senha não pode ser vazia") String senha,
+
+        @Email(message = "O e-mail deve ser válido") String email,
+
+        String telefone,
+
+        @Past(message = "A data de nascimento deve ser no passado") LocalDate dataNascimento,
+
+        @NotBlank(message = "O CPF não pode ser vazio") @CPF(message = "O CPF fornecido é inválido") String cpf) {
+}
