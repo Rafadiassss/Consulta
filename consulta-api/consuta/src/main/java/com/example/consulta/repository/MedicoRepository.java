46de2c43639077b83690574c7d954d749e48c081
LuisHRodrigues <luishenriquebr505@gmail.com>
2025-06-12 16:41:01 -0300

    Adiciona um metodo para verificar se tem algum medico usando alguma especialidade

--- consulta-api/consuta/src/main/java/com/example/consulta/repository/MedicoRepository.java
+++ consulta-api/consuta/src/main/java/com/example/consulta/repository/MedicoRepository.java
@@ -4,4 +4,7 @@ import com.example.consulta.model.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    // Adiciona uma verificação para saber se tem algum medico usando alguma
+    // especialidade
+    boolean existsByEspecialidadeId(Long especialidadeId);
 }
